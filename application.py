c1d01aa40ee0cdb73c7caba286551f3e9bc1b06b
janina3 <janina3@icloud.com>
2018-05-06 18:22:16 -0400

    nav bar

--- application.py
+++ application.py
@@ -4,13 +4,13 @@ import pymysql
 application = Flask(__name__)
 
 conn = pymysql.connect(host='dbinstance.clvo2ema2nfj.us-east-2.rds.amazonaws.com',
-					   port = 3306,
-                       user='checkupdb',
-                       password='Strauss4life',
-                       db='checkupdb',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor
-                       )
+						port = 3306,
+						user='checkupdb',
+						password='Strauss4life',
+						db='checkupdb',
+						charset='utf8mb4',
+						cursorclass=pymysql.cursors.DictCursor
+						)
 
 @application.route('/')
 def start():
@@ -24,35 +24,125 @@ def start():
 
 @application.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-    # grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-    user_id = request.form['user_id']
+	# grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+	user_id = request.form['user_id']
 
-    # cursor used to send queries
-    cursor = conn.cursor()
-    # executes query
-    query = "INSERT INTO login_credentials VALUES (%s,%s,%s)"
-    cursor.execute(query, (username, password, user_id))
-    conn.commit()
+	# cursor used to send queries
+	cursor = conn.cursor()
+	# executes query
+	query = "INSERT INTO login_credentials VALUES (%s,%s,%s)"
+	cursor.execute(query, (username, password, user_id))
+	conn.commit()
 
-    cursor.close()
-    return redirect("/")
+	cursor.close()
+	return redirect("/")
+
+@application.route('/handle_upload', methods=['GET', 'POST'])
+def handle_upload():
+	# print("upload()")
+	userID = 1
+
+	#TODO: implement correct user ID
+
+
+	heightFT = request.form["height-ft"]
+	heightIN = request.form["height-in"]
+	weight = request.form["weight"]
+	bloodPressure = request.form["bloodPressure"]
+	heartRate = request.form["heartRate"]
+	bloodSugar = request.form["bloodSugar"]
+
+	#TODO: fix height
+
+	#temp values for until the front end is fixed
+	# weight = "0.00"
+	# heightFT = "0.00"
+	# heightIN = "0.00"
+	# bloodSugar= "0.00"
+	# bloodPressure = "0.00"
+	# heartRate = "0.00"
+
+	#temp values for until the front end is fixe
+	cursor = conn.cursor()
+
+	query = "INSERT INTO data (userID, data, TypeID) VALUES (%s,%s,%s)"
+	cursor.execute(query, (userID, weight, 0))
+	conn.commit()
+
+	query = "INSERT INTO data (userID, data, TypeID) VALUES (%s,%s,%s)"
+	cursor.execute(query, (userID, heightFT, 1))
+	conn.commit()
+
+	query = "INSERT INTO data (userID, data, TypeID) VALUES (%s,%s,%s)"
+	cursor.execute(query, (userID, heartRate, 2))
+	conn.commit()
+
+	query = "INSERT INTO data (userID, data, TypeID) VALUES (%s,%s,%s)"
+	cursor.execute(query, (userID, bloodPressure, 3))
+	conn.commit()
+
+	query = "INSERT INTO data (userID, data, TypeID) VALUES (%s,%s,%s)"
+	cursor.execute(query, (userID, bloodSugar, 4))
+	conn.commit()
+
+	cursor.close()
+	return render_template('upload.html')
 
 @application.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'GET':
-        return render_template('upload.html')
+	if request.method == 'GET':
+		return render_template('upload.html')
 
 @application.route('/signin', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
 
+@application.route('/processSignIn', methods=['GET', 'POST'])
+def processSignIn():
+	email = request.form["inputEmail"]
+	password = request.form["inputPassword"]
+
+	cursor = conn.cursor()
+	query = "SELECT * FROM accounts WHERE userName = %s and userPass = %s"
+	cursor.execute(query, (email, password))
+	data = cursor.fetchone()
+	cursor.close()
+
+	error = None
+	if (data):
+		session['username'] = email
+		return redirect(url_for("graphs"))
+	else:
+		error = "The username and password are incorrect. Please try again."
+		return render_template('login.html', error = error)
+
 @application.route('/graphs', methods=['GET', 'POST'])
 def graphs():
-    if request.method == 'GET':
-        return render_template('graphs.html')
+	if request.method == 'GET':
+		cursor = conn.cursor()
+		query = "SELECT data FROM data WHERE userID = 1 AND typeID = 3"
+		cursor.execute(query)
+		data = cursor.fetchall()
+		yValues = []
+		for j in data:
+			print(j['data'])
+			yValues.append(j['data'])
+		print()
+		cursor = conn.cursor()
+		query = "SELECT * FROM data WHERE userID = 1 AND typeID = 3"
+		cursor.execute(query)
+		data = cursor.fetchall()
+		xValues = []
+		for j in data:
+			xValues.append(j['‘date’'])
+			print(j['‘date’'])
+
+
+		legend = 'Data'
+		return render_template('graphs.html', values=yValues, labels=xValues, legend = legend)
 
 @application.route('/signup')
 def signup():
@@ -65,21 +155,22 @@ def handle_signup():
 	firstname = request.form["first_name"]
 	lastname = request.form["last_name"]
 	email = request.form["email"]
+
+	#TODO: add hashing to password
 	password = request.form["password"]
 	pass_conf = request.form["password_confirmation"]
 
-	#temp values for until the front end is fixed
-	username = "default"
-	DOB = "00/00/00"
-	sex = "defualt"
+
+	username = email
+	DOB = request.form["DOB"]
+	sex = request.form["sex"]
 	gender = "default"
 	password_match = True
-	
+
+
 	if (password != pass_conf):
 		password_match = False
-		#do something
-		#prob need to pass in a variable "error" into 
-		#signin.html and have it make the user sign up again
+		#TODO: show an error if the passwords do not match
 
 	cursor = conn.cursor()
 	query = "INSERT INTO patient (firstName, lastName, email, dob, sex, gender) VALUES (%s,%s,%s,%s,%s,%s)"
@@ -91,8 +182,8 @@ def handle_signup():
 	conn.commit()
 
 	cursor.close()
-	return render_template('login.html')
-	
+	return render_template('login.html', password_match = password_match)
+
 
 @application.route('/files', methods=['GET', 'POST'])
 def files():
@@ -103,7 +194,7 @@ def files():
 def settings():
     if request.method == 'GET':
         return render_template('settings.html')
-            
+
 #Log out
 @application.route('/logout', methods = ['GET', 'POST'])
 def logout():
@@ -111,7 +202,7 @@ def logout():
         return redirect(url_for('home'))
 
 
-
+application.secret_key = 'Yekaterina Petrovna Zamolodchikova, but you can call me Katya!'
 
 # run the app.
 if __name__ == "__main__":
